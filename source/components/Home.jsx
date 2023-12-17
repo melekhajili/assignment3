e2024c117492680ba0ed4a96b0d0e14e86ed798c
Melek Hajili <95547000+melekhajili@users.noreply.github.com>
2023-12-17 15:03:20 +0400

    Commit for Homepage

---
+++ source/components/Home.jsx
@@ -0,0 +1,26 @@
+import { projects } from '../../constants';
+
+const ProjectCard = ({ project }) => {
+    return (
+        <div className="project-card">
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+            <a target="_blank" href={project.link} rel='external'>Learn More</a>
+        </div>
+    );
+};
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>My Projects</h1>
+            <div className="project-list">
+                {projects.map((project, index) => (
+                    <ProjectCard key={`project-${index}`} project={project} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ source/constants.js
@@ -0,0 +1,5 @@
+export const projects = [
+  { name: 'Project', description: 'Description of Project', link: 'http://link-to-project.com' },
+  { name: 'Project', description: 'Description of Project', link: 'http://link-to-project.com' },
+  { name: 'Project', description: 'Description of Project', link: 'http://link-to-project.com' },
+];
